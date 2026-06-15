f4c0ed6ac3ac541a0280b7ad5931cc8d30067093
ivan-dev <ivanallen64@gmail.com>
2026-06-15 17:58:07 +0800

    Added active and add to cart button simplified

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -78,7 +78,7 @@ function Home() {
                         Unique vintage and thrifted pieces added to our collection this week.
                     </p>
 
-                    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-12 w-full">
+                    <div className="grid grid-cols-2 lg:grid-cols-3 gap-6 mt-12 w-full">
                         {featuredProducts.map((product) => (<ProductCard key={product.id} product={product}/>))}
                     </div>
 
